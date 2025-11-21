2aee45b9e5009e6a5d2a9ac1d6ec42309039a728
pujitha1244 <pujithavankadari1244@gmail.com>
2025-11-21 13:36:16 +0530

    Premium added

--- src/components/Premium.jsx
+++ src/components/Premium.jsx
@@ -1,8 +1,18 @@
 import axios from "axios";
-import React from "react";
+import React, { useState } from "react";
 import { BASE_URL } from "../utils/contants";
 
 const Premium = () => {
+  const [isUserPremium, setIsUserPremium] = useState(false);
+  const verifyPremiumUser = async () => {
+    const res = await axios.get(BASE_URL + "/premium/verify", {
+      withCredentials: true,
+    });
+    if (res.data.isPremium) {
+      setIsUserPremium(true);
+    }
+  };
+
   const handleBuyClick = async (type) => {
     const order = await axios.post(
       BASE_URL + "/payment/create",
@@ -19,15 +29,21 @@ const Premium = () => {
       name: "Dev Tinder",
       description: "Connect to other Developers",
       order_id: orderId, // This is the order_id created in the backend
-    //   callback_url: "http://localhost:3000/payment-success", // Your success URL
+      //   callback_url: "http://localhost:3000/payment-success", // Your success URL
       prefill: {
-        name: notes.firstName+ " "+ notes.lastName,
+        name: notes.firstName + " " + notes.lastName,
         email: notes.emailId,
         contact: "9999999999",
       },
       theme: {
         color: "#F37254",
       },
+      //   handler: function (response) {
+      //     alert(response.razorpay_payment_id);
+      //     alert(response.razorpay_order_id);
+      //     alert(response.razorpay_signature);
+      //   },
+      handler: verifyPremiumUser,
     };
 
     //It Should Open Razorpay Dialog Box
@@ -35,7 +51,11 @@ const Premium = () => {
     rzp.open();
   };
 
-  return (
+  return isUserPremium ? (
+    <div>
+      <h1>You are already a premium Member</h1>
+    </div>
+  ) : (
     <div className="m-10">
       <div className="flex w-full">
         <div className="card bg-base-300 rounded-box grid h-80 grow place-items-center">
